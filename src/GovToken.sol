8db53b2d3fac514cf4990ec2c84fa8918e284cf3
SquilliamX <smartchainwizz@gmail.com>
2023-09-09 16:16:46 -0400

    Updated ReadMe, src, and tests!

--- src/GovToken.sol
+++ src/GovToken.sol
@@ -5,10 +5,15 @@ import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {ERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 import {ERC20Votes} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
 
-contract MyToken is ERC20, ERC20Permit, ERC20Votes {
+contract GovToken is ERC20, ERC20Permit, ERC20Votes {
     constructor() ERC20("MyToken", "MTK") ERC20Permit("MyToken") {}
 
     // The following functions are overrides required by Solidity.
+
+    //mint function allows anyone to mint- this is for testing purposes only!
+    function mint(address to, uint256 amount) public {
+        _mint(to, amount);
+    }
 
     function _afterTokenTransfer(address from, address to, uint256 amount) internal override(ERC20, ERC20Votes) {
         super._afterTokenTransfer(from, to, amount);
